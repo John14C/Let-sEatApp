827ddb5445eabafeec59900b8246f0b0fb6c9c2f
John14C <jakub.cyndrowski@o2.pl>
2024-02-07 17:33:24 +0100

    Initial

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -120,13 +120,12 @@ class recipeView extends View {
   }
 
   _generateMarkupIngredient(ing) {
+    console.log(ing);
     return ` <li class="recipe__ingredient"> 
     <svg class="recipe__icon">
       <use href="${icons}#icon-check"></use>
     </svg>
-    <div class="recipe__quantity">${
-      ing.quantity ? new Fraction(ing.quantity).toString() : ''
-    }</div>
+    <div class="recipe__quantity">${ing.quantity ? ing.quantity : ''}</div>
     <div class="recipe__description">
       <span class="recipe__unit">${ing.unit}</span>
       ${ing.description}
